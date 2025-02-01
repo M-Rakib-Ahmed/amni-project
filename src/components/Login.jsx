d5cffa33053182fdf36ae552030e5ffb8b420f26
M Rakib Ahmed <ahmedmrakib24@gmail.com>
2025-02-01 15:24:16 +0600

    singin added

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 
 
 const Login = () => {
@@ -15,13 +16,14 @@ const Login = () => {
             <form onSubmit={handleLogin} className="card-body">
                 <fieldset className="fieldset">
                     <label className="fieldset-label">Email</label>
-                    <input name="email" type="email" className="input" placeholder="Email" />
+                    <input name="email" type="email" className="input" placeholder="Enter your email" />
                     <label className="fieldset-label">Password</label>
-                    <input name="password" type="password" className="input" placeholder="Password" />
+                    <input name="password" type="password" className="input" placeholder="Enter your password" />
                     <div><a className="link link-hover">Forgot password?</a></div>
                     <button className="btn btn-neutral mt-4">Login</button>
                 </fieldset>
             </form>
+            <p className="text-center pb-5">New to this website? please <Link className="text-green-700 underline" to={'/register'}>Register</Link></p>
         </div>
 
 
--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,18 +1,28 @@
+import { Link } from "react-router-dom";
 
 
 const Register = () => {
+    const handleRegister = (e)=>{
+        e.preventDefault()
+        const email = e.target.email.value;
+        console.log(email);
+        const password = e.target.password.value;
+        console.log(password);
+    
+    }
     return (
         <div className="card bg-base-100 w-full mx-auto max-w-sm shrink-0 shadow-2xl">
-            <form className="card-body">
+            <form onSubmit={handleRegister} className="card-body">
                 <fieldset className="fieldset">
                     <label className="fieldset-label">Email</label>
-                    <input type="email" className="input" placeholder="Enter you email" />
+                    <input name="email" type="email" className="input" placeholder="Enter you email" />
                     <label className="fieldset-label">Password</label>
-                    <input type="password" className="input" placeholder="Enter your password" />
+                    <input name="password" type="password" className="input" placeholder="Enter your password" />
                     <div><a className="link link-hover">Forgot password?</a></div>
                     <button className="btn btn-neutral mt-4">Register</button>
                 </fieldset>
             </form>
+            <p className="text-center pb-5">Already have an account? please<Link className="text-green-700 underline" to={'/login'}>Login</Link></p>
         </div>
     );
 };
